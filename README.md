f45fc71c703112df6dadbe33788aa1260228e511
lsoulima <47644158+lsoulima@users.noreply.github.com>
2021-08-23 20:08:41 +0100

    Register & Login Pages

---
+++ README.md
@@ -0,0 +1,3 @@
+## Animated Form
+
+Build an animated signup form with HTML & CSS, then add a password strength calculator with Svelte. 
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Login from "./Login.svelte";
+  import Register from "./Register.svelte";
+</script>
+
+<style>
+  main {
+    display: flex;
+    align-items: center;
+	flex-direction: column;
+	font-family: sofia-pro, sans-serif;
+	text-align: center;
+  }
+
+  h1 {
+    font-family: gothic-open-shaded, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style>
+
+<main>
+
+  <h1>Sign In</h1>
+  <Login />
+  <h1>Sign Up</h1>
+  <Register />
+</main>
